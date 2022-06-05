b89f9f0b512595486cd3e71858bb1cc5e3833972
albatool <a23@test.com>
2022-06-05 16:32:48 +0300

    first commit

---
+++ python.py
@@ -0,0 +1,53 @@
+
+
+# 1 Basic
+
+# for x in range(0, 151, 1):
+#     print(x)
+
+
+#2 Multiples of Five 
+
+# for y in range(5, 1001, 5):
+#     print(y)
+
+
+# 3 Counting, the Dojo Way 
+# for y in range(1,101):
+#     if y%10  == 0: 
+#         print("coding Dojo")
+#     elif y%5 == 0:
+#         print( "Dojo")
+
+
+# 4 Whoa. That Sucker's Huge
+
+# sum=0
+# for x in range(0, 500001 ):
+#     if (x % 2) != 0:
+#         sum+=x 
+        
+# print(sum)
+
+
+# 5 Countdown by Fours
+# for x in range(2018,0,-4):
+#     print(x)
+
+
+
+# 6 Flexible Counter 
+
+lowNum= 2
+highNum= 9
+mult= 3
+
+for x in range(lowNum,highNum+1,1):
+    if x%mult == 0:
+        print(x)
+
+
+    
+
+
+
